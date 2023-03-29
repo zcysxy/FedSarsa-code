68a2ecb053187abf4703ecfa7113f820b3e61168
zcysxy <zcysxy.wyzdz@gmail.com>
2023-03-29 17:39:57 -0400

    markov_gen

--- markov_gen.m
+++ markov_gen.m
@@ -1,27 +1,20 @@
-%% Markov Matrix Generator %%
-%-----------------------------%
-% Takes as input # of states, discount factor, dimension of feature matrix
-% Returns Markov matrix + Reward Vector + True theta +stationary dist
-% + feature matrix
+%% Markov Matrix Generator
+function [P,R]= markov_gen(S, Rmax)
+    % Generate a Markov Matrix 
+    % Takes as input # of states, discount factor
+    % Returns Markov matrix + Reward Vector
+    
+    % Reward vector (on state alone)
+    R = rand(S,1) * Rmax;
 
-function [theta_t,P,R,p]= markov_gen(S,Rmax, gamma,phi)
-% Generate a Markov Matrix 
-%--------------------------------------------------------------------------
-P=rand(S,S);
-P = P ./ sum(P, 2);
-
-% Reward vector
-R = rand(S,1) * Rmax;
-
-[~,~,W]=eig(P);
-
-% Extracting the stationary distribution
-p=W(:,1); p=p.*sign(p); p=p/sum(p);
-D = diag(p);
-
-
-% remove this (no close solution for SARSA)
-Proj=phi*inv(phi'*D*phi)*phi'*D; %#ok<MINV> 
-A=(eye(S,S)-gamma*Proj*P)*phi;
-b=Proj*R;
-theta_t=A\b; % fixed point of TD.
+    P0 = rand(S,S);
+    P0 = P0 ./ sum(P0, 2);
+    P = @P_gen;
+    function Pa = P_gen(a)
+        a1 = int16(a * S^2);
+        i = a1 / S;
+        j = a1 - i * S;
+        Pa = circshift(P0, i);
+        Pa = circshift(Pa, j, 2);
+    end
+end
